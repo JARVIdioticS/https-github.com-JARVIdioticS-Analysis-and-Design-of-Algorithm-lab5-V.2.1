2d2d46a8c6acd6cfd4866c1dcf52c2a72aa14e28
JARVIdioticS <114250375+JARVIdioticS@users.noreply.github.com>
2023-02-28 16:15:33 +0700

    Update knapsack.java

--- knapsack.java
+++ knapsack.java
@@ -6,6 +6,8 @@ public class knapsack {
     private static void knapsackSort(int amount, double[] value, double[] weight, double sackCap)
     {
         formatter(value, weight);
+        //System.out.println("\n" + Arrays.toString(value));
+        //System.out.println("\n" + Arrays.toString(weight));
         double[] valueByWeight = new double[amount];
         for(int i = 0; i < amount; i++){
             valueByWeight[i] = value[i] / weight[i];
@@ -21,11 +23,18 @@ public class knapsack {
                     swap(valueByWeight, i, j);
                 }
             }
-            
             //put in bag
-            if (sackCap <= weight[i])
+            if (sackCap <= weight[i] || i == amount - 1 || Double.isNaN(valueByWeight[i+1]))
             {
-                total += value[i] * sackCap / weight[i];
+                if (sackCap > weight[i]) {
+                    sackCap -= weight[i];
+                    total += value[i];
+                }
+                else {
+                    total += value[i] * sackCap / weight[i];
+                    sackCap = 0;
+                }
+                
                 System.out.printf("\n%-15s %-15s %-15s %-15s\n", "Order", "Value", "Weight", "Quantity");
                 for (int n = 0; n < i ; n++)
                 {
@@ -33,27 +42,30 @@ public class knapsack {
                 }
                 System.out.printf("%-15d %-15.2f %-15.2f %-15s\n", i+1, value[i], weight[i], sackCap + "/" + weight[i]);
                 System.out.println("Total value is " + total);
+                if (sackCap > 0) System.out.println("There is space left equal to: " + sackCap);
                 return;
             }
             sackCap -= weight[i];
             total += value[i];
         }
-        //System.out.println("\n" + Arrays.toString(valueByWeight) + "\n" + total);
+        System.out.println("\n" + Arrays.toString(valueByWeight) + "\n" + total + " something went wrong...");
     }
     
-    private void formatter(double[] value, double[] weight)
+    private static void formatter(double[] value, double[] weight)
     {
         for(int i = 0; i < weight.length; i++){
             if(weight[i] <= 0){
                 shifter( value, i);
                 shifter( weight, i);
+                i--;
             }
         }
     }
     
-    private void shifter(double[] arr, int startIndex)
+    private static void shifter(double[] arr, int startIndex)
     {
-        
+        for(int i = startIndex; i < arr.length-1; i++) arr[i] = arr[i+1];
+        arr[arr.length - 1] = Double.NaN;
     }
     
     private static void swap(double[] arr, int i, int j)
@@ -88,5 +100,4 @@ public class knapsack {
 
         knapsackSort(amount, value, weight , sackCap);
     }
-    
 }
